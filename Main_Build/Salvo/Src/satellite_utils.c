792c54d5378776e901cce71d2cbb8dc21adafc37
Buneabt <buneabt@yahoo.com>
2024-10-14 19:47:26 -0400

    Fixed error regarding system crashing, forgot to reset binary semaphore for the TaskComm once used.

--- Main_Build/Salvo/Src/satellite_utils.c
+++ Main_Build/Salvo/Src/satellite_utils.c
@@ -1,29 +1,40 @@
 #include "satellite_utils.h"
+#include <salvo.h>  
+
+#define SEC_PER_DAY   (24 * 60 * 60)
+#define SEC_PER_HOUR  (60 * 60)
+#define SEC_PER_MIN   60
+#define HOUR_PER_DAY  24
+#define MIN_PER_HOUR  60
+#define MIN_PER_DAY   (24 * 60)
+
 
 int checkBatteryLevel(void) {
     // Generate a random number between 0 and 100
     return rand() % 101;
 }
 
-//char * time_elapsed_DDHHMMSSTT (void) {
-//    OStypeTick sec, tt;
-//    int dd, hh, mm, ss;
-//    static char str[15];
-//    
-//    tt = OSGetTicks();
-//    sec = (tt / TICKS_PER_SEC);
-//    dd = (sec / SEC_PER_DAY);
-//    hh = (sec / SEC_PER_HOUR) - (DD * HOUR_PER_DAY);
-//    mm = (sec / SEC_PER_MIN) - (hh * MIN_PER_HOUR) - ( dd * MIN_PER_DAY);
-//    ss = sec - (mm * SEC_PER_MIN) - (hh * SEC_PER_HOUR) - (dd * SEC_PER_DAY);
-//    tt = tt%TICKS_PER_SEC;
-//    dd %= 100;
-//    sprintf(str, "%02d:%02d:%02d:%02")
-//    
-//    
-//    
-//}
-
-
-
-// Add other utility function implementations here in the future
+const char* time_elapsed_DDHHMMSSTT(void) {
+    static char str[48];  // Increased size for debug info
+    OStypeTick ticks;
+    unsigned long long sec;
+    int dd, hh, mm, ss;
+    
+    ticks = OSGetTicks();
+    sec = ticks;  // Since each tick is now a second
+    
+    dd = sec / SEC_PER_DAY;
+    sec %= SEC_PER_DAY;
+    
+    hh = sec / SEC_PER_HOUR;
+    sec %= SEC_PER_HOUR;
+    
+    mm = sec / SEC_PER_MIN;
+    ss = sec % SEC_PER_MIN;
+    
+    dd %= 100;  // Ensure days don't exceed two digits
+    
+    sprintf(str, "%02d:%02d:%02d:%02d (Ticks: %lu)", dd, hh, mm, ss, ticks);
+    
+    return str;
+}
